57ebe885d96903931762e7b9b91e5de47c77ae8b
maerd19 <revolutions_333@hotmail.com>
2020-09-23 07:41:38 -0500

    Creando la app y primeros pasos

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styled from "@emotion/styled";
+
+const Heading = styled.h1`
+  color: red;
+`;
+
+export default function Home() {
+  return (
+    <div>
+      <Heading>Inicio</Heading>
+    </div>
+  );
+}
