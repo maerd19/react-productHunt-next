83cecc71e256f26c4ce66f4b1a7a2858d2c9094f
maerd19 <revolutions_333@hotmail.com>
2020-09-23 08:01:52 -0500

    Navegacion y Elementos en el header

---
+++ components/layout/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Buscar from "./../ui/Buscar";
+import Navegacion from "./Navegacion";
+import Link from "next/Link";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <p>P</p>
+
+          <Buscar />
+
+          <Navegacion />
+        </div>
+
+        <div>
+          <p>Hola: Ivan</p>
+
+          <button type="button">Cerrar Sesion</button>
+
+          <Link href="/">Login</Link>
+          <Link href="/">Crear Cuenta</Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Link from "next/Link";
+import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <h1>Header</h1>
-
-      <nav>
-        <Link href="/">Inicio</Link>
-        <Link href="/nosotros">Nosotros</Link>
-      </nav>
+      <Header />
 
       <main>{children}</main>
     </>
