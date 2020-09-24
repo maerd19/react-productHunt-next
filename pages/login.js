122782b6b5989fba5f05fce6f5dd63f3da9c8759
maerd19 <revolutions_333@hotmail.com>
2020-09-24 08:06:21 -0500

    Inicio de sesion

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import { css } from "@emotion/core";
+import Router from "next/router";
 import Layout from "./../components/layout/Layout";
+import {
+  Formulario,
+  Campo,
+  InputSubmit,
+  Error,
+} from "./../components/ui/Formulario";
+
+import firebase from "./../firebase";
+
+// Validaciones
+import useValidacion from "./../hooks/useValidacion";
+import validarIniciarSesion from "./../validacion/validarIniciarSesion";
+
+const STATE_INICIAL = {
+  email: "",
+  password: "",
+};
 
 export default function Login() {
+  // State del componente
+  const [error, setError] = useState(false);
+
+  const {
+    valores,
+    errores,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+  } = useValidacion(STATE_INICIAL, validarIniciarSesion, iniciarSesion);
+  const { email, password } = valores;
+
+  async function iniciarSesion() {
+    try {
+      await firebase.login(email, password);
+      Router.push("/");
+    } catch (error) {
+      console.log("Hubo un error al iniciar sesion", error.message);
+      setError(error.message);
+    }
+  }
   return (
     <div>
       <Layout>
-        <h1>Login</h1>
+        <>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            Iniciar Sesion
+          </h1>
+          <Formulario onSubmit={handleSubmit} noValidate>
+            <Campo>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Tu Email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+
+            {errores.email && <Error>{errores.email}</Error>}
+
+            <Campo>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Tu Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+
+            {errores.password && <Error>{errores.password}</Error>}
+
+            {error && <Error>{error}</Error>}
+
+            <InputSubmit type="submit" value="Iniciar Sesion" />
+          </Formulario>
+        </>
       </Layout>
     </div>
   );
---
+++ validacion/validarIniciarSesion.js
@@ -0,0 +1,18 @@
+// Reglas de validacion para crear-cuenta
+export default function validarIniciarSesion(valores) {
+  let errores = {};
+
+  // Validar email del usuario
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(valores.email)) {
+    errores.email = "Email no valido";
+  }
+  if (!valores.email) errores.email = "El email es obligatorio";
+
+  // Validar password del usuario
+  if (!valores.password) errores.password = "El password es obligatorio";
+  if (valores.password.length < 6) {
+    errores.password = "El password debe ser de al menos 6 caracteres";
+  }
+
+  return errores;
+}
