0da5554bd7462de9426f7271b0b6125361f2bebf
maerd19 <revolutions_333@hotmail.com>
2020-09-25 14:42:44 -0500

    Mostrar productos en la pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Layout from "./../components/layout/Layout";
+import { FirebaseContext } from "./../firebase";
+import DetallesProducto from "./../components/layout/DetallesProducto";
 
 export default function Home() {
+  // State
+  const [productos, setProductos] = useState([]);
+  // Context
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      // Query a la BD's de firestore
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        // snapshot es la forma por la que se accede a los registros
+        .onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        // Acceso a los datos de cada uno de los documentos
+        ...doc.data(),
+      };
+    });
+
+    setProductos(productos);
+  }
+
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map((producto) => (
+                <DetallesProducto key={producto.id} producto={producto} />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
