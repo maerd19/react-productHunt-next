0a9e081718086d43ae0ee680de59e2522283e5b1
maerd19 <revolutions_333@hotmail.com>
2020-09-26 22:54:29 -0500

    Validar existencia de producto

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,20 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useRouter } from "next/router";
 
+import { FirebaseContext } from "./../../firebase";
+import Layout from "./../../components/layout/Layout";
+import Error404 from "./../../components/layout/404";
+
 const Producto = () => {
+  // State del componente
+  const [producto, setProducto] = useState({});
+  const [error, setError] = useState(false);
+
   // Routing para obtener el id actual
   const router = useRouter();
   const {
     query: { id },
   } = router;
 
+  // context de firebase
+  const { firebase } = useContext(FirebaseContext);
+
+  // useEffect se usa para evitar fallos en la consulta en la BD's
   useEffect(() => {
     if (id) {
-      console.log("Ya hay un id", id);
+      const obtenerProducto = async () => {
+        const productoQuery = await firebase.db.collection("productos").doc(id);
+        const producto = await productoQuery.get();
+        if (producto.exists) {
+          setProducto(producto.data());
+        } else {
+          setError(true);
+        }
+      };
+      obtenerProducto();
     }
-  }, []);
+  }, [id]);
 
-  return <h1>Desde {id}</h1>;
+  return (
+    <Layout>
+      <>{error && <Error404 />}</>
+    </Layout>
+  );
 };
 
 export default Producto;
